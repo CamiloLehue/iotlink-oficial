8e0e3c2e464a1d0851f307580347de010290eee9
Camilo Lehue <clehue@ast.cl>
2025-04-09 17:19:57 -0400

    server 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    server: {
+      allowedHosts: ['iotlink.cl'],
+    },
     plugins: [tailwindcss()]
   },
 
